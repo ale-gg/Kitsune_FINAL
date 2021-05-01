dae909195fc2d9cf4df87007307932f1389ac0a1
Eric.H24 <eric.hernandez@enti.cat>
2021-05-01 11:58:25 +0200

    arboles y setas
    las setas te hacen saltar mas alto

---
+++ Assets/Scrpits/SETA.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SETA : MonoBehaviour
+{
+public CharacterController2D player;
+
+public float FuerzaSalto;
+
+  
+
+      void OnTriggerEnter2D(Collider2D obj){
+        if(obj.tag=="Player"){
+             
+             player.m_JumpForce=FuerzaSalto;
+           
+            
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D obj){
+        if(obj.tag=="Player"){
+             
+             player.m_JumpForce=300f;
+           
+            
+        }
+    }
+    
+}
